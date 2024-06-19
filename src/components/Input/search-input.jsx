d0cf156cc3097081db51efcd87bbc72b6bdda06b
mayspiek <maya.spieker@gmail.com>
2024-06-19 12:03:45 -0300

    feat: search input com url state

---
+++ src/components/Input/search-input.jsx
@@ -0,0 +1,28 @@
+import { InputAdornment, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import React, { useState } from 'react'
+import { useQuery } from "react-query";
+import { useSearchParams } from "react-router-dom";
+
+
+export const SearchInput = () => {
+    const [searchParams, setSearchParams] = useSearchParams({
+        search: '',
+    });
+    const search = searchParams.get('search');
+
+    return (
+        <TextField
+            label='Pesquisar paciente'
+            size='small'
+            sx={'width: 100%; background-color: var(--blueish-gray); border-radius: var(--border-radius)'}
+            value={search}
+            onChange={e => setSearchParams({ search: e.target.value }, { replace: true })}
+            InputProps={{
+                endAdornment:
+                    <InputAdornment>
+                        <SearchIcon />
+                    </InputAdornment>
+            }} />
+    )
+}
