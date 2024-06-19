5e099e5303f4cbb9e850b21bdb541548f402cc8c
mayspiek <maya.spieker@gmail.com>
2024-06-19 17:25:47 -0300

    style: arrumando props do botao

--- src/components/button/button.style.ts
+++ src/components/button/button.style.ts
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
-export const ButtonStyle = styled.button`
-    background-color: ${props => props.color || "var(--red)"};
+type ButtonStyleProps = {
+    color?: string;
+    bgColor?: string;
+}
+
+export const ButtonStyle = styled.button<ButtonStyleProps>`
+    background-color: ${props => props.bgColor || "var(--red)"};
     padding: .7rem 0;
     border-radius: var(--border-radius);
     font-weight: var(--semi-bold);
-    color: var(--white);
+    color: ${props => props.color || "var(--white)"};
     width: 100%;
     text-align: center;
     transition: all .3s;
