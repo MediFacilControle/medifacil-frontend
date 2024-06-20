6135afd82fd83e5b8a1fa0d6c6966c2a472609ef
mayaspiek <maya.spieker@gmail.com>
2024-06-20 20:11:42 -0300

    fix: cagadas da gabi

--- src/assets/api/AxiosHttpClient.js
+++ src/assets/api/AxiosHttpClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const authApi = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://medifacil-backend.vercel.app/",
     headers: {
         "Content-Type": "application/json",
     },
