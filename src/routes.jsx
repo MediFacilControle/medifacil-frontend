e81a3c4d6915fd5bd39d9e4326de8106942913b4
mayspiek <maya.spieker@gmail.com>
2024-06-21 17:13:03 -0300

    feat: private route

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,18 +5,21 @@ import { CadastroReceita } from "./pages/cadastro-receita/cadastro-receita";
 import { CadastroSaude } from "./pages/cadastro-saude/cadastro-saude";
 import { PageNotFound } from "./pages/not-found/not-found";
 import { Home } from "./pages/home/home";
+import { PrivateRoute } from "./private-route";
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route exact path="/" element={<Login />} /> 
-                <Route exact path="/home" element={<Home />} />
-                <Route exact path="/cadastro-saude" element={<CadastroSaude />} />
-                <Route exact path="/cadastro-paciente" element={<CadastroPaciente />} /> 
-                <Route exact path="/cadastro-receita" element={<CadastroReceita />} />
+                <Route exact path="/" element={<Login />} />
+                <Route element={<PrivateRoute />}>
+                    <Route exact path="/home" element={<Home />} />
+                    <Route exact path="/cadastro-saude" element={<CadastroSaude />} />
+                    <Route exact path="/cadastro-paciente" element={<CadastroPaciente />} />
+                    <Route exact path="/cadastro-receita" element={<CadastroReceita />} />
+                </Route>
 
-                <Route path="*" element={<PageNotFound />} /> 
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
         </BrowserRouter>
     )
